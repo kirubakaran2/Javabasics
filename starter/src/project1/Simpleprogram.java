4dd48df76d1e7c3558f1f563246ac35294e54a88
kirubakaran <72642390+kirubakaran2@users.noreply.github.com>
2024-06-27 11:03:47 +0530

    commit

---
+++ starter/src/project1/Simpleprogram.java
@@ -0,0 +1,12 @@
+
+package project1;
+
+public class Simpleprogram {
+	public Simpleprogram() {	
+	}
+	public static void main(String[] args) {
+		System.out.println("California");
+		System.out.println("Pondy");
+	}
+
+}
